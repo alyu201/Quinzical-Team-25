d05929f01e30e43a9428490771b2f063e1c84635
wqsz7xn <wqsz7xn@protonmail.com>
2020-10-01 14:49:23 +1300

    Correctly enable and disable buttons

--- src/controller/PointsPlayController.java
+++ src/controller/PointsPlayController.java
@@ -1,9 +1,9 @@
 package controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
-import javafx.beans.binding.Bindings;
 import model.QuinzicalTuple;
 import model.GameMode.GameType;
 import javafx.event.Event;
@@ -11,12 +11,10 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import model.MainModel;
 import utilities.SceneManager;
 
@@ -52,8 +50,10 @@ public class PointsPlayController {
 		if (this.model.getGameQuestions().size() == 0) {
 			ArrayList<String> categoriesSet = new ArrayList<String>();
 			Random rand = new Random();
+
 			while (categoriesSet.size() < 5) {
 				int nextRandBounded = Math.abs(rand.nextInt() % (this.model.getCategories().size() - 1));
+				// System.out.println(nextRandBounded);
 				String currentCategory = this.model.getCategories().get(nextRandBounded);
 				if (!categoriesSet.contains(currentCategory)) {
 					categoriesSet.add(currentCategory);
@@ -74,12 +74,13 @@ public class PointsPlayController {
 				int i = 0;
 				while (i < 5) {
 					int nextRandBounded = Math.abs(rand.nextInt() % (filteredQuestions.size() - 1));
-					System.out.println("" + nextRandBounded);
+					// System.out.println(nextRandBounded);
 					QuinzicalTuple currentQuestion = filteredQuestions.get(nextRandBounded);
 					if (!questionSetCurrentCategory.contains(currentQuestion)) {
 						questionSetCurrentCategory.add(currentQuestion);
 						++i;
 					}
+					System.out.println("" + i);
 					questionSet.addAll(questionSetCurrentCategory);
 				}
 
@@ -90,37 +91,61 @@ public class PointsPlayController {
 				}
 			}
 		}
-		
-		
+
 		ArrayList<String> questionCategories = new ArrayList<String>();
-		for(QuinzicalTuple question : this.model.getGameQuestions()) {
-			if(!questionCategories.contains(question.getCategory())) {
+		for (QuinzicalTuple question : this.model.getGameQuestions()) {
+			if (!questionCategories.contains(question.getCategory())) {
 				questionCategories.add(question.getCategory());
 			}
 		}
-		
+
 		int r = 0;
 		int c = 0;
-		for(String category : questionCategories) {
+		for (String category : questionCategories) {
+			boolean flag = false;
 			Label label = new Label(category);
 			label.setTextFill(Color.web("#FF0000"));
 			this.gridPanePoints.add(label, c, r);
 			r++;
 			ArrayList<QuinzicalTuple> filteredQuestions = new ArrayList<QuinzicalTuple>();
-			for(QuinzicalTuple question : this.model.getGameQuestions()) {
-				if(!filteredQuestions.contains(question) && category.equals(question.getCategory())) {
+			for (QuinzicalTuple question : this.model.getGameQuestions()) {
+				if (!filteredQuestions.contains(question) && category.equals(question.getCategory())) {
 					filteredQuestions.add(question);
 				}
 			}
-		
-			
-			for(QuinzicalTuple question: filteredQuestions) {
-				Button button = new Button(question.getQuestion());
+
+			// sort questions by worth lowest worth first
+			Collections.sort(filteredQuestions, ((x, y) -> {
+				return Integer.compare(((QuinzicalTuple) x).getWorth(), ((QuinzicalTuple) y).getWorth());
+			}));
+
+			for (QuinzicalTuple question : filteredQuestions) {
+				Button button = new Button("$" + question.getWorth());
+				if (question.getCompleted() == true || flag == true) {
+					button.setDisable(true);
+				}
+
+
+				button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+					@Override
+					public void handle(MouseEvent me) {
+						model.setCompleted(GameType.GAMESMODULE, question);
+						model.setCurrentQuestion(question);
+						model.toJSONFile();
+						SceneManager.changeScene(getClass().getResource("/view/QuestionView.fxml"), me);
+					}
+				});
+				button.setPrefSize(200, 100);
 				this.gridPanePoints.add(button, c, r);
+
+				if(question.getCompleted() == false) {
+					flag = true;
+				}
+
 				++r;
 			}
 			++c;
-			r=0;
+			r = 0;
 		}
 
 	}
